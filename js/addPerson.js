0b843ad038efe3b8be7d4e65e6d056bf3579fcc4
CapnSammehj <samherring111@gmail.com>
2019-02-17 15:12:02 +1000

    RSVP sucks

--- js/addPerson.js
+++ js/addPerson.js
@@ -1,25 +1,12 @@
 let addedPersons = 1;
 function addPerson(){
-  //Find the DIV we want to add things too.
-  var container = document.getElementById("newPerson");
-
-  //Increase the addedPersons value by 1 for each click
+    //Increase the addedPersons value by 1 for each click
   addedPersons++;
-  var div = document.createElement("div");
-  div.setAttribute("class", "form-group");
-  container.appendChild(div);
 
-  var label = document.createElement("label");
-  label.htmlFor = "name" + addedPersons;
-  label.innerHTML = "Full Name " + addedPersons;
-  div.appendChild(label);
+  var element = document.getElementById("name" + addedPersons);
+  element.setAttribute("type", "visible");
 
-  var input = document.createElement("input");
-  input.setAttribute("type", "text");
-  input.setAttribute("class", "form-control");
-  input.id = "name" + addedPersons;
-  input.setAttribute("name", "Full Name " + addedPersons);
-  div.appendChild(input);
+  var label = document.getElementById("name" + addedPersons + "Label");
+  label.removeAttribute("hidden");
 
-  console.log(addedPersons);
 }
